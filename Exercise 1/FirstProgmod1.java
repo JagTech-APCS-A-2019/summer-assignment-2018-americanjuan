e464ba8b66b8267fcdbfd821c9bb62b1f8772673
Joshua Breedy <0197323@students.dadeschools.net>
2018-08-08 12:52:53 -0400

    Add files via upload

---
+++ Exercise 1/FirstProgmod1.java
@@ -0,0 +1,6 @@
+public class FirstProgmod1{
+	public static void main( String[] args ) {
+		System.out.println( "I, Joshua Breedy, am determined to learn how to code." );
+		System.out.println( "Today's date is " );
+	}
+}
---
+++ Exercise 1/FirstProgmod2.java
@@ -0,0 +1,6 @@
+public class FirstProgmod2{
+	public static void main( String[] args ) {
+		System.out.println( "I, Joshua Breedy, am determined to learn how to code." );
+		System.out.println( "Today's date is Monday, July 19, 2018." );
+	}
+}
