c6361ae09a4efad30ff080a5e02a88b815cdf812
Joshua Breedy <0197323@students.dadeschools.net>
2018-08-08 12:54:26 -0400

    Add files via upload

---
+++ Exercise 2/GasolineReceiptmod1.java
@@ -0,0 +1,16 @@
+public class GasolineReceiptmod1 {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      CORNER STORE      |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| 2018-07-18  02:34PM    |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:        10.870 |" );
+		System.out.println( "| Price/gallon: $  2.73  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Fuel total:   $ 29.68  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
